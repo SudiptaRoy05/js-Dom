73860893f677c7c008297c1436499f4b299cca80
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-09-14 23:14:20 +0600

    dom3

--- script.js
+++ script.js
@@ -8,3 +8,13 @@ for (const l of li) {
     console.log(l.innerText);    
 }
 
+document.getElementById('fruitTitle').style.color = "red";
+const title = document.getElementById('fruitTitle');
+title.innerText = "my Tour List" 
+console.log(title.innerText);
+
+
+const imp = document.getElementsByClassName('impPlace');
+for (const i of imp) {
+    console.log(i.innerHTML)
+}
