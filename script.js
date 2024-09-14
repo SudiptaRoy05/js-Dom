8e4c3f64bb382b3387dd21cb122014fe57a52d27
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-09-14 22:56:44 +0600

    dom2

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("Hello from js");
+const h =  document.getElementsByTagName('h1');
+console.log(h);
+
+const li =  document.getElementsByTagName('li');
+
+for (const l of li) {
+    console.log(l.innerText);    
+}
+
